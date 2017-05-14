be223d43fa7be67caefea96d828cbd6d6060f122
Carlos Hernández Juárez <carlos21p6-0800@comunidad.unam.mx>
2017-05-14 16:16:24 -0500

    Add files via upload

--- Proyecto Seguridad/programs/registro.php
+++ Proyecto Seguridad/programs/registro.php
@@ -10,10 +10,10 @@ echo"<!DOCTYPE html>
 			<h1>Registro</h1>
 			<form method='GET' action='registroR.php'>
 				<label>Nombre de usuario</label><br/>
-				<input type=text size='50' maxlength='15' name='usuario' placeholder='Máximo 15 caracteres' required/>
+				<input type=text size='60' maxlength='15' name='usuario' placeholder='Máximo 15 caracteres alfanuméricos (letras, dígitos y @/./+/-/_) solamente.' required/>
 				<br/><br/>
 				<label>Contraseña</label><br/>
-				<input type='password' size='50' maxlength='30' name='contraseña' required/><br/>
+				<input type='password' size='60' maxlength='30' name='contraseña' required/><br/>
 				<label class='cont'>Mínimo 8 caracteres.Longitud máxima: 30 caracteres alfanuméricos (letras, dígitos y @/./+/-/_) solamente.</label>
 				<br/><br/>
 				<input type='submit' value='Registrarse'/>
--- Proyecto Seguridad/programs/sesion.php
+++ Proyecto Seguridad/programs/sesion.php
@@ -8,15 +8,15 @@ echo"<!DOCTYPE html>
 		</head>
 		<body>
 			<h1>Inicio de sesión</h1>
-			<form>
+			<form action='sesionR.php' method='POST'>
 				<label>Nombre de usuario</label><br/>
-				<input type='text' size='30' maxlength='15' required/>
+				<input type='text' size='30' maxlength='15' name='usu' required/>
 				<br/><br/>
 				<label>Contraseña</label><br/>
-				<input type='password' size='30' maxlength='30' required/>
+				<input type='password' size='30' maxlength='30' name='contra' required/>
 				<br/><br/>
 				<input type='submit' value='Iniciar sesión'/>
-				
+			</form>
 			<br/><br/><br/><br/>
 			<div>
 				<a href='inicio.php'>Inicio</a><br/><br/>
